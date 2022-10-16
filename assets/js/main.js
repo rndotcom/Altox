3405ff37c93e1f963df843e06594f4613749ca9f
Rayhan Nabi <me@rayhannabi.com>
2022-10-16 18:10:34 +0600

    Add auto dark mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,63 +1,69 @@
-var html = $('html');
+var html = $("html");
 
 $(function () {
-    darkMode();
-    carousel();
+  darkMode();
+  carousel();
 });
 
 function darkMode() {
-    $('.toggle-track').on('click', function () {
-        if (html.hasClass('dark-mode')) {
-            html.removeClass('dark-mode');
-            localStorage.setItem('alto_dark', false);
-        } else {
-            html.addClass('dark-mode');
-            localStorage.setItem('alto_dark', true);
-        }
-    });
+  function toggleDarkMode(state) {
+    if (state) {
+      html.addClass("dark-mode");
+      localStorage.setItem("alto_dark", true);
+    } else {
+      html.removeClass("dark-mode");
+      localStorage.setItem("alto_dark", false);
+    }
+  }
+
+  const useDark = window.matchMedia("(prefers-color-scheme: dark)");
+  useDark.addEventListener("change", (event) => toggleDarkMode(event.matches));
+  $(".toggle-track").on("click", () =>
+    toggleDarkMode(!html.hasClass("dark-mode"))
+  );
 }
 
 function carousel() {
-    var carousel = $('.carousel');
-    var postImage = carousel.find('.post-image');
-    var imageHeight, nav;
+  var carousel = $(".carousel");
+  var postImage = carousel.find(".post-image");
+  var imageHeight, nav;
 
-    function moveNav() {
-        imageHeight = postImage.height();
-        if (!nav) {
-            nav = carousel.find('.owl-prev, .owl-next');
-        }
-        nav.css({
-            top: imageHeight / 2 + 'px',
-            opacity: 1,
-        });
+  function moveNav() {
+    imageHeight = postImage.height();
+    if (!nav) {
+      nav = carousel.find(".owl-prev, .owl-next");
     }
-
-    carousel.owlCarousel({
-        dots: false,
-        margin: 28,
-        nav: true,
-        navText: [
-            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="22" height="22" fill="currentColor"><path d="M20.547 22.107l-6.107-6.107 6.107-6.12-1.88-1.88-8 8 8 8 1.88-1.893z"></path></svg>',
-            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="22" height="22" fill="currentColor"><path d="M11.453 22.107l6.107-6.107-6.107-6.12 1.88-1.88 8 8-8 8-1.88-1.893z"></path></svg>',
-        ],
-        onInitialized: function () {
-            moveNav();
-            carousel.css('visibility', 'visible');
-        },
-        onResized: function () {
-            moveNav();
-        },
-        responsive: {
-            0: {
-                items: 1,
-            },
-            768: {
-                items: 3,
-            },
-            992: {
-                items: 4,
-            },
-        },
+    nav.css({
+      top: imageHeight / 2 + "px",
+      opacity: 1,
     });
+  }
+
+  carousel.owlCarousel({
+    dots: false,
+    margin: 28,
+    nav: true,
+    navText: [
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="22" height="22" fill="currentColor"><path d="M20.547 22.107l-6.107-6.107 6.107-6.12-1.88-1.88-8 8 8 8 1.88-1.893z"></path></svg>',
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="22" height="22" fill="currentColor"><path d="M11.453 22.107l6.107-6.107-6.107-6.12 1.88-1.88 8 8-8 8-1.88-1.893z"></path></svg>',
+    ],
+    onInitialized: function () {
+      moveNav();
+      carousel.css("visibility", "visible");
+    },
+    onResized: function () {
+      moveNav();
+    },
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 3,
+      },
+      992: {
+        items: 4,
+      },
+    },
+  });
 }
